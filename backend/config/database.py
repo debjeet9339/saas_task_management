60cdfa68dac6d50bf116d69a9d2b8b0a2c7bd381
debjeet9339 <debjeethalder9339@gmail.com>
2026-05-26 09:14:56 +0530

    commit 4

---
+++ backend/config/database.py
@@ -0,0 +1,11 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.config.settings import settings
+
+
+client = AsyncIOMotorClient(settings.MONGO_URL)
+database = client[settings.DATABASE_NAME]
+
+
+users_collection = database["users"]
+workspaces_collection = database["workspaces"]
+tasks_collection = database["tasks"]
